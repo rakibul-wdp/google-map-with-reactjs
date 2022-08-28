c0db4ed65697ef296c3ed99669e1125ea818381d
rakibul-wdp <rakibul.wdp@gmail.com>
2022-08-28 21:36:01 +0600

    project almost done

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
+  // const [clickZoom, setClickZoom] = useState('');
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -29,8 +30,16 @@ function App() {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
+      // setClickZoom(map.current.addControl(new mapboxgl.NavigationControl()));
     });
   });
+
+  // map.addControl(
+  //   new MapboxDirections({
+  //     accessToken: mapboxgl.accessToken,
+  //   }),
+  //   'top-left'
+  // );
 
   return (
     <div>
